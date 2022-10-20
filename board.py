17a86dece3837a2376aae45fbf25ab39ff38e649
Mason Acevedo <macevedo@hmc.edu>
2022-10-19 22:39:50 -0700

    More refactoring (supposed to be same commit as last!)
    Rewrote calculateBestMove(),
    
    Started rewriting simulate()
    
    added constants to Board.py file

--- board.py
+++ board.py
@@ -1,6 +1,106 @@
 import copy
 import random
-import numpy as np
+from Moves import Move
+
+
+# How do we want to deal with the weights? 
+# Is this an object of type board?
+# On the one hand, it is a grid of 16 values,
+# similar to a board. On the other hand, 
+# it does not -- and should not -- 
+# support the same functionality: finding the 
+# best move, simulating an arbitrary sequence of 
+# moves, etc... 
+SNAKE_WEIGHTS =  [[3**1, 3**2, 3**3, 3**4],
+            [3**8, 3**7, 3**6, 3**5],
+            [3**9, 3**10,3**11,3**12],
+            [3**16,3**15,3**14,3**13]]
+
+PRECOMPUTED_SEQUENCES = {
+1: [[Move.UP], [Move.RIGHT], [Move.DOWN], [Move.LEFT]],
+
+2: [[Move.UP, Move.UP],
+ [Move.UP, Move.RIGHT],
+ [Move.UP, Move.DOWN],
+ [Move.UP, Move.LEFT],
+ [Move.RIGHT, Move.UP],
+ [Move.RIGHT, Move.RIGHT],
+ [Move.RIGHT, Move.DOWN],
+ [Move.RIGHT, Move.LEFT],
+ [Move.DOWN, Move.UP],
+ [Move.DOWN, Move.RIGHT],
+ [Move.DOWN, Move.DOWN],
+ [Move.DOWN, Move.LEFT],
+ [Move.LEFT, Move.UP],
+ [Move.LEFT, Move.RIGHT],
+ [Move.LEFT, Move.DOWN],
+ [Move.LEFT, Move.LEFT]],
+
+
+ 3: [[Move.UP, Move.UP, Move.UP],
+ [Move.UP, Move.UP, Move.RIGHT],
+ [Move.UP, Move.UP, Move.DOWN],
+ [Move.UP, Move.UP, Move.LEFT],
+ [Move.UP, Move.RIGHT, Move.UP],
+ [Move.UP, Move.RIGHT, Move.RIGHT],
+ [Move.UP, Move.RIGHT, Move.DOWN],
+ [Move.UP, Move.RIGHT, Move.LEFT],
+ [Move.UP, Move.DOWN, Move.UP],
+ [Move.UP, Move.DOWN, Move.RIGHT],
+ [Move.UP, Move.DOWN, Move.DOWN],
+ [Move.UP, Move.DOWN, Move.LEFT],
+ [Move.UP, Move.LEFT, Move.UP],
+ [Move.UP, Move.LEFT, Move.RIGHT],
+ [Move.UP, Move.LEFT, Move.DOWN],
+ [Move.UP, Move.LEFT, Move.LEFT],
+ [Move.RIGHT, Move.UP, Move.UP],
+ [Move.RIGHT, Move.UP, Move.RIGHT],
+ [Move.RIGHT, Move.UP, Move.DOWN],
+ [Move.RIGHT, Move.UP, Move.LEFT],
+ [Move.RIGHT, Move.RIGHT, Move.UP],
+ [Move.RIGHT, Move.RIGHT, Move.RIGHT],
+ [Move.RIGHT, Move.RIGHT, Move.DOWN],
+ [Move.RIGHT, Move.RIGHT, Move.LEFT],
+ [Move.RIGHT, Move.DOWN, Move.UP],
+ [Move.RIGHT, Move.DOWN, Move.RIGHT],
+ [Move.RIGHT, Move.DOWN, Move.DOWN],
+ [Move.RIGHT, Move.DOWN, Move.LEFT],
+ [Move.RIGHT, Move.LEFT, Move.UP],
+ [Move.RIGHT, Move.LEFT, Move.RIGHT],
+ [Move.RIGHT, Move.LEFT, Move.DOWN],
+ [Move.RIGHT, Move.LEFT, Move.LEFT],
+ [Move.DOWN, Move.UP, Move.UP],
+ [Move.DOWN, Move.UP, Move.RIGHT],
+ [Move.DOWN, Move.UP, Move.DOWN],
+ [Move.DOWN, Move.UP, Move.LEFT],
+ [Move.DOWN, Move.RIGHT, Move.UP],
+ [Move.DOWN, Move.RIGHT, Move.RIGHT],
+ [Move.DOWN, Move.RIGHT, Move.DOWN],
+ [Move.DOWN, Move.RIGHT, Move.LEFT],
+ [Move.DOWN, Move.DOWN, Move.UP],
+ [Move.DOWN, Move.DOWN, Move.RIGHT],
+ [Move.DOWN, Move.DOWN, Move.DOWN],
+ [Move.DOWN, Move.DOWN, Move.LEFT],
+ [Move.DOWN, Move.LEFT, Move.UP],
+ [Move.DOWN, Move.LEFT, Move.RIGHT],
+ [Move.DOWN, Move.LEFT, Move.DOWN],
+ [Move.DOWN, Move.LEFT, Move.LEFT],
+ [Move.LEFT, Move.UP, Move.UP],
+ [Move.LEFT, Move.UP, Move.RIGHT],
+ [Move.LEFT, Move.UP, Move.DOWN],
+ [Move.LEFT, Move.UP, Move.LEFT],
+ [Move.LEFT, Move.RIGHT, Move.UP],
+ [Move.LEFT, Move.RIGHT, Move.RIGHT],
+ [Move.LEFT, Move.RIGHT, Move.DOWN],
+ [Move.LEFT, Move.RIGHT, Move.LEFT],
+ [Move.LEFT, Move.DOWN, Move.UP],
+ [Move.LEFT, Move.DOWN, Move.RIGHT],
+ [Move.LEFT, Move.DOWN, Move.DOWN],
+ [Move.LEFT, Move.DOWN, Move.LEFT],
+ [Move.LEFT, Move.LEFT, Move.UP],
+ [Move.LEFT, Move.LEFT, Move.RIGHT],
+ [Move.LEFT, Move.LEFT, Move.DOWN],
+ [Move.LEFT, Move.LEFT, Move.LEFT]]}
 
 class Board:
     
@@ -25,26 +125,167 @@ class Board:
 
         return formattedString
     
+def calculateBestMove(currentBoard, ply):
+    """
+    Given the current state of the game, and a "ply"
+    value, which is how many steps into the future
+    to look ahead, this function returns the best 
+    move. 
 
-    def validMove(currentBoard, move):
-        if (simulate(currentBoard, move) == "NO MOVE TO SIMULATE"):
-            return False
-        else:
-            return True
+    Specifically, we define the "best" move as a 
+    max-min of all the possibilities:
+    If we look at the worst-case scenario for
+    moving up, down, left, or right, which of 
+    these moves has the best worst-case scenario?
+    That's the one we return. 
+    """
+    # First, get all possible sequences of 
+    # 4 moves. (There are 4^ply of these)
+    sequences = getSequences(ply)
 
-    # Seems like we should probably get rid of this function...
-    def deepMax(L):
-        if ((len(L) == 1) and (type(L[0]) == int)):
-            return L[0]
-        elif ((len(L) != 1) and (type(L[0]) == int)):
-            return max(L[0],deepMax(L[1:]))
-        elif ((len(L) == 1)):
-            return deepMax(L[0])
-        else:
-            return max(deepMax(L[0]), deepMax(L[1:]))
+    worstOutcomes = []
+    for sequence in sequences:
+        firstMove = sequence[0]
+        if validMove(firstMove):
+            possibleOutcomes = simulateSequence(currentBoard, sequence)
+            scores = [evaluateBoard(outcome) for outcome in possibleOutcomes]
+            worstOutcomes.append((min(scores), firstMove))
+    score, move = min(worstOutcomes)
+
+    return move
+
+def getSequences(n):
+    """
+    Given a positive integer n,
+    this function returns all 
+    the possible sequences of n
+    moves.
+
+    Note that the vast majority of the time,
+    we're expecting n=1,2, or 3. 4 and
+    above is rare enough that we 
+    won't bother. 
+    """
+
+    if n in PRECOMPUTED_SEQUENCES:
+        return PRECOMPUTED_SEQUENCES[n]
+    else:
+        shorterSequences = getSequences(n-1)
+        ans = []
+        for sequence in shorterSequences:    
+            for move in Move:
+                ans.append(sequence + [move])
+        return ans
+
+
+def simulateSequence(self, moveList):
+    """
+    Inputs: 
+        A list of moves, and the current 
+        state of the board. 
+    Outputs: 
+        All the possible outcomes after
+        those moves have been played. 
+    """
+    if len(moveList) == 1:
+        move = moveList[0]
+        
+
+
+# Code is extremely NOT DRY! Need to fix! 
+def simulate(self, move):
+
+    if (move == "Down"):
+        # move tiles down
+        newBoard = shiftDown(board)
+        # merge if necessary
+        for columnIndex in range(0,4):
+            doubledRows = []
+            for rowIndex in range(1,4):
+                if (newBoard[rowIndex-1][columnIndex] == newBoard[rowIndex][columnIndex] and newBoard[rowIndex][columnIndex] != 0):
+                    doubledRows.append(rowIndex)
+            for index in range(0,len(doubledRows)):
+                newBoard[doubledRows[index]][columnIndex] = 2*newBoard[doubledRows[index]][columnIndex]
+                newBoard[doubledRows[index]-1][columnIndex] = 0
+        # after merging, move down again
+        newBoard = shiftDown(newBoard)
+    elif (move == "Up"):
+        # move tiles down
+        newBoard = shiftUp(board)
+        # merge if necessary
+        for columnIndex in range(0,4):
+            doubledRows = []
+            for rowIndex in range(0,3):
+                if (newBoard[rowIndex][columnIndex] == newBoard[rowIndex+1][columnIndex] and newBoard[rowIndex][columnIndex] != 0):
+                    doubledRows.append(rowIndex)
+            for index in range(0,len(doubledRows)):
+                newBoard[doubledRows[index]][columnIndex] = 2*newBoard[doubledRows[index]][columnIndex]
+                newBoard[doubledRows[index]+1][columnIndex] = 0
+        # after merging, move down again
+        newBoard = shiftUp(newBoard)
+
+    elif (move == "Left"):
+        # move tiles down
+        newBoard = shiftLeft(board)
+        # merge if necessary
+        for rowIndex in range(0,4):
+            doubledCols = []
+            for colIndex in range(0,3):
+                if (newBoard[rowIndex][colIndex] == newBoard[rowIndex][colIndex+1] and newBoard[rowIndex][colIndex] != 0):
+                    doubledCols.append(colIndex)
+            for index in range(0,len(doubledCols)):
+                newBoard[rowIndex][doubledCols[index]] = 2*newBoard[rowIndex][doubledCols[index]]
+                newBoard[rowIndex][doubledCols[index]+1] = 0
+        # after merging, move down again
+        newBoard = shiftLeft(newBoard)
+
+    else: # move == "Right"
+        # move tiles down
+        newBoard = shiftRight(board)
+        # merge if necessary
+        for rowIndex in range(0,4):
+            doubledCols = []
+            for colIndex in range(1,4):
+                if (newBoard[rowIndex][colIndex-1] == newBoard[rowIndex][colIndex] and newBoard[rowIndex][colIndex] != 0):
+                    doubledCols.append(colIndex)
+            for index in range(0,len(doubledCols)):
+                newBoard[rowIndex][doubledCols[index]] = 2*newBoard[rowIndex][doubledCols[index]]
+                newBoard[rowIndex][doubledCols[index]-1] = 0
+        # after merging, move down again
+        newBoard = shiftRight(newBoard)
+    
+    if (newBoard == board):
+        return "NO MOVE TO SIMULATE"
+    else:
+        return newBoard
+
+def addRandomTile(self):
+    """ Adds a 2 or 4 to the board in a random, empty tile. """
+    
+    # before adding a random tile,
+    # we have to figure out which
+    # tiles are empty.
+    emptyCoordinates = []
+    for outerIndex in range(0, len(self.tiles)):
+        for innerIndex in range(0, len(self.tiles[0])):
+            if self.tiles[outerIndex][innerIndex] == 0:
+                emptyCoordinates.append((outerIndex, innerIndex))
+
+    row, col = random.choice(emptyCoordinates)
+    addedValue = random.choice([2,4])
+    self.tiles[row][col] = addedValue
+
+def validMove(self, move):
+    if (self.simulate(move) == "NO MOVE TO SIMULATE"):
+        return False
+    else:
+        return True
 
 
 
+
+def shift(self, direction):
+    pass
 # This code is NOT DRY AT ALL. Gotta figure out 
 # a way to make it DRY. 
 def shiftDown(board):
@@ -132,83 +373,7 @@ def shiftRight(board):
     return newBoard
 
 
-
-
-# Similar Dryness Problem Here
-
-def simulate(board, move):
-
-    if (move == "Down"):
-        # move tiles down
-        newBoard = shiftDown(board)
-        # merge if necessary
-        for columnIndex in range(0,4):
-            doubledRows = []
-            for rowIndex in range(1,4):
-                if (newBoard[rowIndex-1][columnIndex] == newBoard[rowIndex][columnIndex] and newBoard[rowIndex][columnIndex] != 0):
-                    doubledRows.append(rowIndex)
-            for index in range(0,len(doubledRows)):
-                newBoard[doubledRows[index]][columnIndex] = 2*newBoard[doubledRows[index]][columnIndex]
-                newBoard[doubledRows[index]-1][columnIndex] = 0
-        # after merging, move down again
-        newBoard = shiftDown(newBoard)
-    elif (move == "Up"):
-        # move tiles down
-        newBoard = shiftUp(board)
-        # merge if necessary
-        for columnIndex in range(0,4):
-            doubledRows = []
-            for rowIndex in range(0,3):
-                if (newBoard[rowIndex][columnIndex] == newBoard[rowIndex+1][columnIndex] and newBoard[rowIndex][columnIndex] != 0):
-                    doubledRows.append(rowIndex)
-            for index in range(0,len(doubledRows)):
-                newBoard[doubledRows[index]][columnIndex] = 2*newBoard[doubledRows[index]][columnIndex]
-                newBoard[doubledRows[index]+1][columnIndex] = 0
-        # after merging, move down again
-        newBoard = shiftUp(newBoard)
-
-    elif (move == "Left"):
-        # move tiles down
-        newBoard = shiftLeft(board)
-        # merge if necessary
-        for rowIndex in range(0,4):
-            doubledCols = []
-            for colIndex in range(0,3):
-                if (newBoard[rowIndex][colIndex] == newBoard[rowIndex][colIndex+1] and newBoard[rowIndex][colIndex] != 0):
-                    doubledCols.append(colIndex)
-            for index in range(0,len(doubledCols)):
-                newBoard[rowIndex][doubledCols[index]] = 2*newBoard[rowIndex][doubledCols[index]]
-                newBoard[rowIndex][doubledCols[index]+1] = 0
-        # after merging, move down again
-        newBoard = shiftLeft(newBoard)
-
-    else: # move == "Right"
-        # move tiles down
-        newBoard = shiftRight(board)
-        # merge if necessary
-        for rowIndex in range(0,4):
-            doubledCols = []
-            for colIndex in range(1,4):
-                if (newBoard[rowIndex][colIndex-1] == newBoard[rowIndex][colIndex] and newBoard[rowIndex][colIndex] != 0):
-                    doubledCols.append(colIndex)
-            for index in range(0,len(doubledCols)):
-                newBoard[rowIndex][doubledCols[index]] = 2*newBoard[rowIndex][doubledCols[index]]
-                newBoard[rowIndex][doubledCols[index]-1] = 0
-        # after merging, move down again
-        newBoard = shiftRight(newBoard)
-    
-    if (newBoard == board):
-        return "NO MOVE TO SIMULATE"
-    else:
-        return newBoard
-
-
-
-
-# 
-
-
-def addTiles(board):
+def addTiles(self):
     # returns a list containing all the possibilities
     # of where a new square can be added and what that square might be
     possibilitiesList = []
@@ -231,131 +396,38 @@ def addTiles(board):
 
 
 
-def arbitraryWeightsMetric(board, weights):
+def evaluateBoard(board):
     total = 0
     for rowIndex in range(0, len(board)):
         for colIndex in range(0, len(board[0])):
-            total += (weights[rowIndex][colIndex] * board[rowIndex][colIndex])
+            total += (SNAKE_WEIGHTS[rowIndex][colIndex] * board[rowIndex][colIndex])
     return total
 
 
 
-
-def addRandomTile(board):
-    zeroCoordinates = []
-    for outerIndex in range(0, len(board)):
-        for innerIndex in range(0, len(board[0])):
-            if board[outerIndex][innerIndex] == 0:
-                zeroCoordinates.append((outerIndex, innerIndex))
-
-    coordinatesToAddTo = random.choice(zeroCoordinates)
-    addedValue = random.choice([2,4])
-    board[coordinatesToAddTo[0]][coordinatesToAddTo[1]] = addedValue
-
-
-
-
-
-def calculateBestSequenceSnake(currentBoard, lookAhead, weights):
-    if (lookAhead == 1):
-
-        leftMove = simulate(currentBoard, "Left")
-        rightMove = simulate(currentBoard, "Right")
-        upMove = simulate(currentBoard, "Up")
-        downMove = simulate(currentBoard, "Down")
-
-        moves = [leftMove, rightMove, upMove, downMove]
+# # inputs: a sequence of moves
+# # outputs: the average snake metric if you were to do those moves
+# def evaluateSequenceSnake(currentBoard, sequence, weights):
+#     if (len(sequence) == 0):
+#         return -1
+#     elif (validMove(currentBoard, sequence[0]) == False):
+#         return -1
+#     elif (len(sequence) == 1):
         
-        allowedMoves = ["NO"]*4
-        for index in range(0, len(moves)):
-            if (moves[index] != "NO MOVE TO SIMULATE"):
-                allowedMoves[index] = moves[index]
+#         move = simulate(currentBoard, sequence[0])
+        
+#         possibilities = addTiles(move)
+#         possibleMetrics = [arbitraryWeightsMetric(board, weights) for board in possibilities]
+#         return np.min(possibleMetrics)
+#     else:
+#         move = simulate(currentBoard, sequence[0])
+#         possibilities = addTiles(move)
 
-        metrics = [-1]*4
+#         averagesList = []
+#         for index in range(0, len(possibilities)):
+#             averagesList.append(evaluateSequenceSnake(possibilities[index], sequence[1:], weights))
 
-        for index in range(0, len(allowedMoves)):
-            if (allowedMoves[index] != "NO"):
-                possibilities = addTiles(allowedMoves[index])
-                possibleMetrics = [arbitraryWeightsMetric(board, weights) for board in possibilities]    
-                metrics[index] = np.min(possibleMetrics)
-            else:
-                metrics[index] = -1
-        
-        
-
-        if (metrics.index(max(metrics)) == 0):
-            bestMove = "Left"
-        elif (metrics.index(max(metrics)) == 1):
-            bestMove = "Right"
-        elif (metrics.index(max(metrics)) == 2):
-            bestMove = "Up"
-        else:
-            bestMove = "Down"
-        
-        return [bestMove]
-    else:
-        if (validMove(currentBoard, "Left")):
-            bestLeft = ["Left"] + calculateBestSequenceSnake(simulate(currentBoard, "Left"), lookAhead-1, weights)
-        else:
-            bestLeft = []
-        
-        if (validMove(currentBoard, "Right")):
-            bestRight = ["Right"] + calculateBestSequenceSnake(simulate(currentBoard, "Right"), lookAhead-1, weights)
-        else:
-            bestRight = []
-        
-        if (validMove(currentBoard, "Up")):
-            bestUp = ["Up"] + calculateBestSequenceSnake(simulate(currentBoard, "Up"), lookAhead-1, weights)
-        else:
-            bestUp = []
-
-        if (validMove(currentBoard, "Down")):
-            bestDown = ["Down"] + calculateBestSequenceSnake(simulate(currentBoard, "Down"), lookAhead-1, weights)
-        else:
-            bestDown = []
-        
-        
-        leftResult = evaluateSequenceSnake(currentBoard, bestLeft, weights)
-        rightResult = evaluateSequenceSnake(currentBoard, bestRight, weights)
-        upResult = evaluateSequenceSnake(currentBoard, bestUp, weights)
-        downResult = evaluateSequenceSnake(currentBoard, bestDown, weights)
-
-        results = [leftResult, rightResult, upResult, downResult]
-        
-        bestIndex = results.index(max(results))
-        if (bestIndex == 0):
-            return bestLeft
-        elif (bestIndex == 1):
-            return bestRight
-        elif (bestIndex == 2):
-            return bestUp
-        else:
-            return bestDown
-
-
-# inputs: a sequence of moves
-# outputs: the average snake metric if you were to do those moves
-def evaluateSequenceSnake(currentBoard, sequence, weights):
-    if (len(sequence) == 0):
-        return -1
-    elif (validMove(currentBoard, sequence[0]) == False):
-        return -1
-    elif (len(sequence) == 1):
-        
-        move = simulate(currentBoard, sequence[0])
-        
-        possibilities = addTiles(move)
-        possibleMetrics = [arbitraryWeightsMetric(board, weights) for board in possibilities]
-        return np.min(possibleMetrics)
-    else:
-        move = simulate(currentBoard, sequence[0])
-        possibilities = addTiles(move)
-
-        averagesList = []
-        for index in range(0, len(possibilities)):
-            averagesList.append(evaluateSequenceSnake(possibilities[index], sequence[1:], weights))
-
-        return np.min(averagesList)
+#         return np.min(averagesList)
 
 def validMove(currentBoard, move):
     if (simulate(currentBoard, move) == "NO MOVE TO SIMULATE"):

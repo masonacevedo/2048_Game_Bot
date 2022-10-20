9e88e20ea2aa08f0fadd6ace38d768e7c91b4453
Mason Acevedo <macevedo@hmc.edu>
2022-10-19 22:37:09 -0700

    More Refactoring
    Rewrote calculateBestMove function.
    
    Began re-wrting simullate () function.

--- Moves.py
+++ Moves.py
@@ -1,7 +1,10 @@
 from enum import Enum
 
 class Move(Enum):
-    UP = auto()
-    DOWN = auto()
-    LEFT = auto()
-    RIGHT= auto()
+    # these numbers don't mean
+    # anything, python just 
+    # requires them to have values
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
